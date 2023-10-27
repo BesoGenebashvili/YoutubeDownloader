fdd50e47a36153f3983df6ea21c4bd1ef33fb0d7
Beso Genebashvili <genebashvili99@gmail.com>
2023-10-27 23:59:24 +0400

    Ref

--- YoutubeDownloader/YoutubeDownloader/Program.cs
+++ YoutubeDownloader/YoutubeDownloader/Program.cs
@@ -4,6 +4,7 @@ using System.Text.RegularExpressions;
 using YoutubeExplode;
 using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
 
 Console.WriteLine("Hello, World!");
 
@@ -12,61 +13,69 @@ var youtubeClient = new YoutubeClient();
 if (!Directory.Exists("Files"))
     Directory.CreateDirectory("Files");
 
-await DownloadAsync(youtubeClient, "ifgpiGs_4Js", "Files", ContentType.MP3);
+var videoIds = File.ReadAllLines("Favorites-videos.csv")
+                   .Skip(1)
+                   .Where(t => !string.IsNullOrWhiteSpace(t))
+                   .Select(t => t.Split(',')
+                                 .First()
+                                 .Trim())
+                   .ToArray();
 
-static async Task<(bool success, string? filename, string? error)> DownloadAsync(
+foreach (var videoId in videoIds)
+{
+    await DownloadMP3Async(youtubeClient, videoId, "Files");
+}
+
+static string ResolveFilename(string title, VideoId videoId)
+{
+    var newFilename = Regex.Replace(title, @"[\\\/:*?""<>|]", string.Empty);
+
+    return string.IsNullOrWhiteSpace(newFilename)
+                 ? videoId
+                 : newFilename;
+}
+
+/// Returns: fileName
+static async Task<string> DownloadMP3Async(
     YoutubeClient youtubeClient,
     VideoId videoId,
     string folderPath,
-    ContentType contentType,
     CancellationToken cancellationToken = default)
 {
-    try
-    {
-        var video = await youtubeClient.Videos
-                                       .GetAsync(videoId, cancellationToken)
-                                       .ConfigureAwait(false);
+    var video = await youtubeClient.Videos
+                                   .GetAsync(videoId, cancellationToken)
+                                   .ConfigureAwait(false);
 
-        var streamManifest = await youtubeClient.Videos
-                                                .Streams
-                                                .GetManifestAsync(videoId, cancellationToken);
+    var streamManifest = await youtubeClient.Videos
+                                            .Streams
+                                            .GetManifestAsync(videoId, cancellationToken);
 
-        var (fileExtension, streamInfo) = contentType switch
-        {
+    var streamInfo = streamManifest.GetAudioOnlyStreams()
+                                   .GetWithHighestBitrate();
 
-            ContentType.MP3 => ("mp3", streamManifest.GetAudioOnlyStreams()
-                                                     .GetWithHighestBitrate()),
-            ContentType.MP4 => ("mp4", streamManifest.GetMuxedStreams()
-                                                     .Where(s => s.Container == Container.Mp4)
-                                                     .GetWithHighestVideoQuality()),
-            _ => throw new NotImplementedException(nameof(contentType))
-        };
+    var fileName = ResolveFilename(video.Title, videoId);
 
-        var fileName = ResolveFilename(video.Title, videoId);
+    var filePath = Path.Combine(folderPath, $"{fileName}.mp3");
 
-        var filePath = Path.Combine(folderPath, $"{fileName}.{fileExtension}");
+    // TODO: report progress
+    await youtubeClient.Videos
+                       .Streams
+                       .DownloadAsync(
+                            streamInfo, 
+                            filePath, 
+                            cancellationToken: cancellationToken);
 
-        await youtubeClient.Videos
-                           .Streams
-                           .DownloadAsync(streamInfo, filePath, null, cancellationToken);
+    return fileName;
+}
 
-        Console.WriteLine($"Downloaded: {videoId}");
-        return (true, fileName, null);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Error: {videoId}, {ex.Message}");
-        return (false, null, ex.Message);
-    }
-
-    static string ResolveFilename(string title, VideoId videoId)
-    {
-        var newFilename = Regex.Replace(title, @"[\\\/:*?""<>|]", string.Empty);
-
-        return string.IsNullOrWhiteSpace(newFilename)
-                     ? videoId
-                     : newFilename;
-    }
+/// Returns: fileName
+static async Task<string> DownloadMP4Async(
+    YoutubeClient youtubeClient,
+    VideoId videoId,
+    string folderPath,
+    CancellationToken cancellationToken = default)
+{
+    throw new NotImplementedException();
 }
 
 enum ContentType
